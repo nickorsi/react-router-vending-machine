71ee2d837bb901bab8a0331071743f6b1a369f5c
Seth Hobson <sethlawrencehobson@gmail.com>
2024-01-22 18:29:09 -0500

    added cookie photo

--- src/Chips.js
+++ src/Chips.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './Chips.css';
 
 
 const CHIPS_IMAGE =
--- src/Cookies.js
+++ src/Cookies.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './Cookies.css'
+
+const COOKIE_LINK = 'https://images.unsplash.com/photo-1499636136210-6f4ee915583e?q=80&w=2264&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
 
 /**
@@ -17,6 +20,7 @@ import {Link} from 'react-router-dom';
 function Cookies() {
   return (
     <div>
+      <img className ='Cookies-image' src={COOKIE_LINK}></img>
       <Link to='/'>Go Back</Link>
     </div>
   )
